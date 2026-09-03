e84add8f5e03babe5077968ff92eac487e43715b
Cristian Jimenez <122385810+CJimenez77@users.noreply.github.com>
2026-09-03 18:48:25 -0400

    inicializacion de proyecto

---
+++ src/main/java/cl/jimenez/munoz/aguilera/baeza/proyectosemestral/ProyectosemestralApplication.java
@@ -0,0 +1,13 @@
+package cl.jimenez.munoz.aguilera.baeza.proyectosemestral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectosemestralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectosemestralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/jimenez/munoz/aguilera/baeza/proyectosemestral/ProyectosemestralApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.jimenez.munoz.aguilera.baeza.proyectosemestral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectosemestralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
